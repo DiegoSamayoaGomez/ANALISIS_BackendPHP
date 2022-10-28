ffc54f55a3f3594c6f90d0d40409aa4f8a1ff236
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-10-28 09:29:16 -0600

    Correccion ingreso de valores en crearQueja

--- crearQueja.php
+++ crearQueja.php
@@ -9,7 +9,7 @@ $id_categoria = $_POST["id_categoria"];
 $id_usuario = $_POST["id_usuario"];
 
 $query = "INSERT INTO quejas(descripcion, imagen, estado, fecha, id_categoria, id_usuario) 
-VALUES('$descripcion', '$imagen', '$estado', now(), '$id_categoria', $id_usuario')";
+VALUES('$descripcion', '$imagen', '$estado', now(), $id_categoria, $id_usuario)";
 mysqli_query($conexion, $query) or die (mysqli_error($conexion));
 mysqli_close($conexion);
 
