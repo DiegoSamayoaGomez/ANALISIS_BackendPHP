97cdd5477efa186615a51f31200955528b94da58
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-10-28 07:45:52 -0600

    Agregado Inicio de Sesión

---
+++ iniciarSesion.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'conexion.php';
+
+$correo = $_POST["correo"];
+$contrasena = $_POST["contrasena"];
+
+
+$query = "SELECT * FROM usuario WHERE correo = '$correo'AND contrasena = ''$contrasena";
+$resultado = mysqli_query($conexion, $query);
+
+
+if($resultado -> num_rows > 0) {
+    echo "Ingreso exitoso";
+}
+else {
+    echo "Ingreso fallido";
+}
+
+?>
