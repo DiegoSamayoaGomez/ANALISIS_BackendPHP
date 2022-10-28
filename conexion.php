386141b424102af5ec56e14f6e70974c69692b32
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-10-28 11:48:45 -0600

    Cambio de conexión: enlace a cleverCloud

--- conexion.php
+++ conexion.php
@@ -4,7 +4,13 @@
     {
         function Conectar()
         {
-          $mysqli = new mysqli("localhost", "root","","controlquejas");
+          $mysqli = new mysqli("bnltxzejf6qnxll0xjii-mysql.services.clever-cloud.com", //HOST
+           "u9vvluuiobf0rl09",//USUARIO
+           "sn3cwuhPNa27LMV1Sbeh",//Contraseña
+           "bnltxzejf6qnxll0xjii");//Nombre de BDD
+
+           
+          //$mysqli = new mysqli("localhost", "root","","controlquejas");
           
           if($mysqli->connect_errno)
           {
