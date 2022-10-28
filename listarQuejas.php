73e2cc320a437d50a26ffcc21291a07d0e3d6ef2
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-10-28 11:00:45 -0600

    Agregado atenderQuejas / Correción en listarQuejas

--- listarQuejas.php
+++ listarQuejas.php
@@ -2,12 +2,11 @@
 //SELECT * FROM quejas WHERE estado = "EN PROCESO" ORDER BY fecha DESC;
 
 include 'conexion.php';
-$estado = $_POST["estado"];
-$fecha = $_POST["fecha"];
+$estado = $_POST['estado'];
+$fecha = $_POST['fecha'];
 
-$query = "SELECT * FROM quejas WHERE estado = '".$estado."' ORDER BY fecha '".$fecha."'";
+$query = "SELECT * FROM quejas WHERE estado = $estado ORDER BY fecha $fecha";
 
 $resultado = mysqli_query($conexion, $query);
-
 
 ?>
