0de7abeb22ad4ff49b74935a1dc19105f368870a
Diego Samayoa <samayoasg2014@gmail.com>
2022-10-29 14:45:55 -0600

    Corrección recepción de datos desde Java

--- crearQueja.php
+++ crearQueja.php
@@ -1,17 +1,19 @@
 <?php
-include 'conexion.php';
+//include 'conexion.php'
+$conexion = mysqli_connect("localhost", "root","","controlquejas");
 //Codigo
 $descripcion = $_POST["descripcion"];
 $imagen = $_POST["imagen"];
-$estado = $_POST["estado"];
-$fecha = $_POST["fecha"];
+//$estado = $_POST["estado"];
+//$fecha = $_POST["fecha"];
 //$estado= 'EN PROCESO';
 //Fecha
 $id_categoria = $_POST["id_categoria"];
 $id_usuario = $_POST["id_usuario"];
 
-$query = "INSERT INTO quejas(descripcion, imagen, estado, fecha, id_categoria, id_usuario) 
-VALUES('$descripcion', '$imagen', '$estado', '$fecha'', $id_categoria, $id_usuario)";
+$query = "INSERT INTO quejas(descripcion, imagen, estado, fecha, id_categoria, id_usuario)
+
+VALUES('$descripcion', '$imagen', 'EN PROCESO', now(), '$id_categoria', $id_usuario)";
 mysqli_query($conexion, $query) or die (mysqli_error($conexion));
 mysqli_close($conexion);
 
